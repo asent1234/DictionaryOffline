3172b9d60fb10a7bb69696350e554bd913419e58
asent1234 <42815464+asent1234@users.noreply.github.com>
2021-02-02 17:31:05 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
